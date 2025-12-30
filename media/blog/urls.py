af328be0f2dd07a3a829fe34c15bc66dccf3f097
walter <musunguwalter57@gmail.com>
2025-12-30 11:02:51 +0300

    created detail view,url, and html page

--- media/blog/urls.py
+++ media/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path # type: ignore
 from .import views
 
 urlpatterns = [
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('', views.index, name='index'),
 ]
--- media/blog/views.py
+++ media/blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import render, get_object_or_404 
+from .models import Post
 
 # Create your views here.
 def index(request):
-      return render(request, 'blog/index.html')
+      posts = Post.objects.order_by('-created_at')
+      return render(request, 'blog/index.html',{'posts': posts})
+
+def post_detail(request, slug):
+      post = get_object_or_404(Post, slug=slug)  # Fetch post by slug or return 404, preventing errors/crash
+      return render(request, 'blog/post_detail.html', {'post': post})
+
+
+
