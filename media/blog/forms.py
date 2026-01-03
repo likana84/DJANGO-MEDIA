774707cb8574c5a9ecc84951d1742c33702a7b64
walter <musunguwalter57@gmail.com>
2026-01-03 09:56:17 +0300

    applied css to form.py

--- media/blog/forms.py
+++ media/blog/forms.py
@@ -1,9 +1,22 @@
 from django.forms import ModelForm
+from django import forms
 
 from .models import  Comment
 
 class CommentForm(ModelForm):
+
+   
+
+  
     class Meta:
         model = Comment
-        fields = ['name', 'content' ]
-
+        fields = ['name', 'content']
+        widgets = {
+            'name': forms.TextInput(attrs={'style': 'border:none;background-color:transparent;', 'placeholder': 'Your name'}),
+            'content': forms.Textarea(attrs={'style': 'border:none;background-color:pink;', 'rows': 4, 'placeholder': 'Your comment'})
+        }
+  #class Meta:
+        #model = Comment
+        #fields = ['name', 'content' ]
+        #class CommentForm(ModelForm):
+        
