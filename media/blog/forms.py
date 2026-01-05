52a0510db390adb6306d547f4d38e0f55755057a
walter <musunguwalter57@gmail.com>
2026-01-05 11:11:48 +0300

    made changes to forn.py

--- media/blog/forms.py
+++ media/blog/forms.py
@@ -1,22 +1,21 @@
 from django.forms import ModelForm
-from django import forms
+
 
 from .models import  Comment
 
 class CommentForm(ModelForm):
-
-   
-
-  
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        # remove labels and add common styling attrs
+        if 'name' in self.fields:
+            self.fields['name'].label = 'Name'
+            self.fields['name'].widget.attrs.update({'style': 'width:200px;background-color:pink;display:block;' })
+        if 'content' in self.fields:
+            self.fields['content'].label = 'Content'
+            self.fields['content'].widget.attrs.update({'style': 'width:200px;background-color:pink;margin-top:10px;display:block;' })
     class Meta:
         model = Comment
-        fields = ['name', 'content']
-        widgets = {
-            'name': forms.TextInput(attrs={'style': 'border:none;background-color:pink;width:255px;'}),
-            'content': forms.Textarea(attrs={'style': 'border:none;background-color:pink;rows:4;cols:8;display:block;margin-bottom:10px;'}),
-        }
-  #class Meta:
-        #model = Comment
-        #fields = ['name', 'content' ]
-        #class CommentForm(ModelForm):
+        fields = ['name',  'content']
+       
+        
         
